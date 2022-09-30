7c8b7fe363a0cb0b2f75f9d2d5e0f642457c7ecc
Raíssa Juliana <rjgalbino@uniara.edu.br>
2022-09-30 15:07:59 -0300

    Começando a colocar peças no tabuleiro

--- Tela.cs
+++ Tela.cs
@@ -7,17 +7,17 @@ namespace xadrex_console
     {
         public static void imprimirTabuleiro(Tabuleiro tab)
         {
-            for (int i=0; i<tab.linhas; i++)
+            for (int i = 0; i < tab.linhas; i++)
             {
-                for (int j=0; j<tab.colunas; j++)
+                for (int j = 0; j < tab.colunas; j++)
                 {
-                    if (tab.peca(i,j) == null)
+                    if (tab.peca(i, j) == null)
                     {
                         Console.Write("- ");
                     }
-                    else 
+                    else
                     {
-                    Console.Write(tab.peca(i,j) + "  ");
+                        Console.Write(tab.peca(i, j) + "  ");
                     }
                 }
                 Console.WriteLine();
