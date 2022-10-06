ebc8b1f68bd1ca96263a2b257bc286fc5948070a
Raíssa Juliana <rjgalbino@uniara.edu.br>
2022-10-06 16:16:13 -0300

    Implementando a verificacao de xeque

--- Tela.cs
+++ Tela.cs
@@ -15,6 +15,10 @@ namespace xadrex_console
             Console.WriteLine();
             Console.WriteLine("Turno: " + partida.turno);
             Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+            if (partida.xeque)
+            {
+                Console.WriteLine("XEQUE!");
+            }
         }
 
         public static void imprimirPecasCapturadas(PartidaDeXadrez partida)
